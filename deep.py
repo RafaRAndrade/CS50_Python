82e9e3981534f8673b7c7ba45519f3647cbc5b43
Rafaela Rodrigues <rafaelardandrade@gmail.com>
2026-04-22 17:28:29 -0300

    Add files via upload
    "In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No."

---
+++ deep.py
@@ -0,0 +1,7 @@
+#input do usuário ajustado
+answer = input ("What is the answer to the Great Question of Life, the Universe, and Everything? ").title().strip()
+
+if answer == ("42") or answer == ("Forty Two") or answer == ("Forty-Two"):
+    print ("Yes")
+else:
+    print ("No")
